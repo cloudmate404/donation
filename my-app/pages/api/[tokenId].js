61d47ee9e7ddfc6f803eaa6852dcd860af6056e3
cloudmate404 <ogunniyi95@gmail.com>
2022-05-06 23:44:28 +0100

    added api

---
+++ my-app/pages/api/[tokenId].js
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const tokenId = req.query.tokenId;
+
+  const name = `Ukraine Relief Token #${tokenId}`;
+
+  const description =
+    "Ukraine Relief Token is a complimentary token for whoever donates to Ukraine Relief Funds";
+
+  const image =
+    "https://raw.githubusercontent.com/cloudmate404/donation/main/my-app/public/ukraineNFT.svg";
+
+  return res.json({
+    name: name,
+    description: description,
+    image: image,
+  });
+}
