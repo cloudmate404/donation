9669c4a2864ec5c7588c4bd5572f24c75d268fae
cloudmate404 <ogunniyi95@gmail.com>
2022-05-06 23:51:21 +0100

    fixing api

--- my-app/pages/api/nft.js
+++ my-app/pages/api/nft.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const tokenId = req.query.tokenId;
+  const tokenId = req.query.nft;
 
-  const name = `Ukraine Relief Token #${tokenId}`;
+  const name = `Ukraine Relief Token`;
 
   const description =
     "Ukraine Relief Token is a complimentary token for whoever donates to Ukraine Relief Funds";
